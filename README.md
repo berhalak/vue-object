a293ddc73fbc11346e426d4f169ea9e23623c1db
berhalak <jaroslaw.sadzinski@gmail.com>
2020-04-20 07:13:46 +0000

    Gitpod

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-Ready--to--Code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/berhalak/vue-object) 
+
 # vue-object
 
 Treat normal objects as vue components. 
---
+++ test/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    runtimeCompiler: true,
+     devServer: {
+        disableHostCheck: true
+    },
+	// keep class names doesn't work, so remove for now
+	configureWebpack: {
+		optimization: {
+			minimize: false
+		}
+    }
+}
